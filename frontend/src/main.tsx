85636ea416f926d25c41cde3f3ba60863015b51f
Meryem Efe <meryemefe.cs@gmail.com>
2024-12-14 01:24:58 +0100

    configure ag-grid-enterprise

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
+import { AllEnterpriseModule, LicenseManager, ModuleRegistry } from "ag-grid-enterprise";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
+ModuleRegistry.registerModules([AllEnterpriseModule]);
+
+LicenseManager.setLicenseKey("test");
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App/>
+    </StrictMode>,
 )
